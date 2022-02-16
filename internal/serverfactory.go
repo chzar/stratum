196df3252038911a301e462de288d1b502a729b4
chzar <cameron.harrell@bouvet.no>
2022-02-16 21:29:07 +0100

    add basic config loading, basic factory func

--- internal/serverfactory.go
+++ internal/serverfactory.go
@@ -1,7 +1,51 @@
 package internal
 
-import gp "github.com/elazarl/goproxy"
+import (
+	"log"
+	"net/http"
+	"os"
 
-func BuildServer(c *ServerConfig) *gp.ProxyHttpServer {
-	return new(gp.ProxyHttpServer)
+	gp "github.com/elazarl/goproxy"
+)
+
+func BuildServer(c *ServerConfig) (*gp.ProxyHttpServer, error) {
+
+	var StratumAlwaysMitm gp.FuncHttpsHandler = func(host string, ctx *gp.ProxyCtx) (*gp.ConnectAction, string) {
+		return &gp.ConnectAction{Action: gp.ConnectMitm, TLSConfig: gp.TLSConfigFromCA(c.CACert)}, host
+	}
+
+	proxy := gp.NewProxyHttpServer()
+
+	for _, h := range c.Hosts {
+		proxy.OnRequest(gp.DstHostIs(h)).HandleConnect(StratumAlwaysMitm)
+	}
+
+	for _, re := range c.UrlPatterns {
+		proxy.OnRequest(gp.UrlMatches(re)).DoFunc(func(req *http.Request, ctx *gp.ProxyCtx) (*http.Request, *http.Response) {
+			ctx.UserData = re.FindStringSubmatch(req.URL.String())[1]
+			return req, nil
+		})
+	}
+
+	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *gp.ProxyCtx) *http.Response {
+		// short circuit non 200 codes
+		if resp == nil || resp.StatusCode != 200 || ctx.UserData == nil {
+			return resp
+		}
+
+		if ctx.UserData.(string) != "" {
+			filename := "workdir/" + ctx.UserData.(string)
+			f, err := os.Open(filename)
+			if err != nil {
+				resp.Body = NewTeeReadCloser(resp.Body, NewFileStream(filename))
+			} else {
+				ctx.Logf("READING FROM CACHE: %s", filename)
+				resp.Body = f
+			}
+		}
+		return resp
+	})
+	proxy.Verbose = true
+	log.Fatal(http.ListenAndServeTLS(":9443", "server.crt", "server.key", proxy))
+	return new(gp.ProxyHttpServer), nil
 }
--- main.go
+++ main.go
@@ -3,44 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
-	"os"
-	"regexp"
 
 	"github.com/chzar/stratum/v2/internal"
-
-	gp "github.com/elazarl/goproxy"
 )
 
-var StratumMitmConnect = gp.ConnectAction{Action: gp.ConnectMitm, TLSConfig: gp.TLSConfigFromCA(&gp.GoproxyCa)}
-
 func main() {
-	re := regexp.MustCompile("production.cloudflare.docker.com:443/registry-v2/docker/registry/v2/blobs/sha256/.+/(.+)/.*")
-
-	proxy := gp.NewProxyHttpServer()
-	proxy.OnRequest(gp.DstHostIs("production.cloudflare.docker.com:443")).HandleConnect(gp.AlwaysMitm)
-	proxy.OnRequest(gp.UrlMatches(re)).DoFunc(func(req *http.Request, ctx *gp.ProxyCtx) (*http.Request, *http.Response) {
-		ctx.UserData = re.FindStringSubmatch(req.URL.String())[1]
-		return req, nil
-	})
-
-	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *gp.ProxyCtx) *http.Response {
-		// short circuit non 200 codes
-		if resp == nil || resp.StatusCode != 200 || ctx.UserData == nil {
-			return resp
-		}
-
-		if ctx.UserData.(string) != "" {
-			filename := "workdir/" + ctx.UserData.(string)
-			f, err := os.Open(filename)
-			if err != nil {
-				resp.Body = internal.NewTeeReadCloser(resp.Body, internal.NewFileStream(filename))
-			} else {
-				ctx.Logf("READING FROM CACHE: %s", filename)
-				resp.Body = f
-			}
-		}
-		return resp
-	})
-	proxy.Verbose = true
+	c := internal.LoadServerConfig()
+	proxy, _ := internal.BuildServer(c)
 	log.Fatal(http.ListenAndServeTLS(":9443", "server.crt", "server.key", proxy))
 }
